ff6d593dc980fea799c54f8c04fbe587b2c61b5e
unknown <joshhaz2012@gmail.com>
2018-10-22 19:43:28 +0100

    renaming Aux -> ReactAux

--- src/containers/DashBoard/DashBoardBuilder.js
+++ src/containers/DashBoard/DashBoardBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/ReactAux';
 import DashBoard from '../../components/DashBoard/DashBoard';
 
 class DashBoardBuilder extends Component {
