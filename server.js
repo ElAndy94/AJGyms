b600ad0ad2c589712b966c46938a9120f4e6554a
Unknown <joshhaz2012@gmail.com>
2018-10-25 20:26:15 +0100

    Added classes GET endpoint - classes stored in server memory

--- server.js
+++ server.js
@@ -4,8 +4,27 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
+const classes = [
+    {
+        location: 'Market Street',
+        classType: 'Induction only',
+        className: 'Intro class',
+        startTime: '10:00am'
+    },
+    {
+        location: 'Portland Street',
+        classType: 'intermediate',
+        className: 'Mass Building',
+        startTime: '7:00pm'
+    },
+];
+
 app.get('/ping', function (req, res) {
  return res.send('pong');
+});
+
+app.get('/api/classes', (req, res) => {
+    return res.json(classes);
 });
 
 app.listen(process.env.PORT || 8080);
