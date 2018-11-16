e6462fbb1a4ddfbd2157c5a2381a3fcb7005e53b
Andy <andrewpeliza@hotmail.com>
2018-11-16 19:04:37 +0000

    Logger custom

--- backend/utility/logger.js
+++ backend/utility/logger.js
@@ -1,11 +1,20 @@
 const { createLogger, format, transports } = require('winston');
 
+const customLevels = {
+  levels: {
+      accessed: 0,
+      error: 0,
+      warn: 0
+  }
+}
+
 module.exports = createLogger({
   format: format.combine(
     format.simple(),
     format.timestamp(),
     format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`)
   ),
+  levels: customLevels.levels,
   transports: [
     new transports.File({
       maxsize: 5120000,
