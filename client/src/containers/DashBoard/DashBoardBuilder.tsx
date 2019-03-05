9332e0ab0b39bc069bad09973a77f8ca1dc60c08
Andrew <andrewpeliza@hotmail.com>
2019-03-05 16:16:54 +0000

    More JS to TSX changes.

---
+++ client/src/containers/DashBoard/DashBoardBuilder.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Button from '../../components/UI/Button/Button';
+import './DashBoard.scss';
+
+interface Props {
+  isAuthenticated: boolean;
+}
+
+const dashBoardBuilder = (props: Props) => {
+  const [joinNow, setJoinNow] = useState(false);
+
+  const joinNowClicked = () => {
+    setJoinNow(true);
+  };
+
+  if (joinNow === true) {
+    return <Redirect to='/signup' />;
+  }
+
+  return (
+    <React.Fragment>
+      <div className='DashBoard'>
+        <h2>AJ GYMS</h2>
+        <div className='DashBoard__img' />
+        <p>
+          Stop waiting... <br /> make a difference now!
+        </p>
+        <p>Join today and pay no joining fee.</p>
+        <p>Bring a friend, signup and first month becomes FREE!</p>
+        {!props.isAuthenticated ? (
+          <Button btnType='Signup' clicked={() => joinNowClicked()}>
+            Join Now
+          </Button>
+        ) : (
+          <p />
+        )}
+      </div>
+    </React.Fragment>
+  );
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.userId !== ''
+});
+
+export default withRouter(
+  //@ts-ignore
+  connect(
+    mapStateToProps,
+    null
+  )(dashBoardBuilder)
+);
